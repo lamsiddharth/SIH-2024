9e678d82fa43f34101319f3b8d66a282f4baafa3
siddharth pradhan <siddharthpradhan2004@gmail.com>
2024-08-30 20:59:55 +0530

    basic set up

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+app.use(cors());
+
+
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
+
+module.exports = router;
 
